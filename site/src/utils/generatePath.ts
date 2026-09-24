a2e5abe1374be80a1cfb44045dce550b89aef61d
Вадим Раковский <ved456@yandex.ru>
2026-09-24 20:24:20 +0300

    Fix asset paths for Vercel

--- site/src/utils/generatePath.ts
+++ site/src/utils/generatePath.ts
@@ -1,9 +1,5 @@
-import isProd from './isProd'
-
-
 const generatePath = (path: string) => {
-  return (isProd() ? 'https://the-sociophobic.github.io/arhi-tect-urno/' : './arhi-tect-urno') + path
+  return path
 }
-
 
 export default generatePath
